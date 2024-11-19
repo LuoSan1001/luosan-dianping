9a7013080a339ec0b1a1167ea972e52e922097ff
LuoSan1001 <luosan1004@163.com>
2024-11-19 18:01:56 +0800

    完成了登录注册和权限认证功能

--- src/main/java/com/luosan/luosandianping/interceptor/LoginInterceptor.java
+++ src/main/java/com/luosan/luosandianping/interceptor/LoginInterceptor.java
@@ -1,27 +1,22 @@
 package com.luosan.luosandianping.interceptor;
 
-import com.luosan.luosandianping.utils.JwtUtils;
+import com.luosan.luosandianping.dto.UserDTO;
+import com.luosan.luosandianping.utils.UserHolder;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-
 @Component
 public class LoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        String token = request.getHeader("token");
-
-        try {
-            JwtUtils.parse(token);
-        } catch (Exception e) {
-            response.getWriter().write("{message: 'NOT_LOGIN'}");
+        UserDTO userDTO = UserHolder.getUser();
+        if (userDTO == null) {
+            response.setStatus(401);
             return false;
+        } else {
+            return true;
         }
-
-        return true;
     }
 }
---
+++ src/main/java/com/luosan/luosandianping/utils/RedisConstants.java
@@ -0,0 +1,6 @@
+package com.luosan.luosandianping.utils;
+
+public class RedisConstants {
+    public static final String LOGIN_CODE_KEY = "login:code:";
+    public static final String USER_TOKEN_KEY = "user:token:";
+}
